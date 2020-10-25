54fba4b19d03813b9dbd9ef733a8878dcb615ab2
Felroy <felroy01998@gmail.com>
2020-10-25 14:52:46 +0000

    Pong tutorial
    Pong tutorial completed, along with scoreboard

---
+++ Pong/Assets/Scripts/BoundController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //Destroys gameobject if its tag is Ball
+        if (other.gameObject.tag == "Ball")
+        {
+            if (this.transform.position.x >= 7) {
+            ScoreScript.EnemyScore++;
+            
+            }
+            if (this.transform.position.x <= -7) {
+            ScoreScript.PlayerScore++;
+            
+            }
+            Destroy(other.gameObject);
+        
+
+        
+        }
+        
+            
+        
+    }
+}
---
+++ Pong/Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    public static int PlayerScore = 0;
+    public static int EnemyScore = 0;
+    Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = GetComponent<Text> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        score.text = "Player: " + PlayerScore + " - " + "Enemy: " + EnemyScore;
+    }
+}
